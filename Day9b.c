2c0c0885dab50b9118e7ae9c3dc434b35f823952
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-05-19 16:13:44 +0530

    Update Day9b.c

--- Day9b.c
+++ Day9b.c
@@ -1,64 +1,27 @@
 //WACP to print the multiplication table of numbers 1 - 5.
 
 #include <stdio.h>
-
-int main()
-{
-    int n;
-    do
-    {
-        printf("\n1.Table of 1 \n2.Table of 2 \n3.Table of 3 \n4.Table of 4 \n5.Table of 5 \n6.Exit \n ");
-        scanf("%d", &n);
-        switch (n)
-        {
-        case 1:
-        {
-            for (int i = 1; i <= 10; i++)
+#include<stdlib.h>
+int main() {
+   do
+   {
+       int i,n;
+       printf("\n1.Table of 1 \n2.Table of 2 \n3.Table of 3 \n4.Table of 4 \n5.Table of 5 \n6.Exit \n");
+       scanf("%d", &n);
+       if(n==6)
+       {
+           exit(0);
+       }
+       if(n>6)
+       {
+           printf("Enter a valid option!");
+       }
+       if(n<=6)
+       {
+            for(i=1;i<=10;i++)
             {
-                printf("%d ", n * i);
+              printf("%d ",n*i);
             }
-            break;
-        }
-        case 2:
-        {
-            for (int i = 1; i <= 10; i++)
-            {
-                printf("%d ", n * i);
-            }
-            break;
-        }
-        case 3:
-        {
-            for (int i = 1; i <= 10; i++)
-            {
-                printf("%d ", n * i);
-            }
-            break;
-        }
-        case 4:
-        {
-            for (int i = 1; i <= 10; i++)
-            {
-                printf("%d ", n * i);
-            }
-            break;
-        }
-        case 5:
-        {
-            for (int i = 1; i <= 10; i++)
-            {
-                printf("%d ", n * i);
-            }
-            break;
-        }
-        case 6:
-        {
-            exit(0);
-        }
-        default:
-        {
-            printf("Enter a valid option!");
-        }
-        }
-    } while (1);
+       }
+   }while(1);
 }
