80ef7aab92dbf28d87e5b50ad74856b7447a9f72
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-05-21 16:16:55 +0530

    Update Day11b.c

--- Day11b.c
+++ Day11b.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float a,b,c;
+    float r1,r2;
+    float determinant;
+    scanf("%f %f %f", &a,&b,&c);
+    determinant = (b*b) - (4*a*c);
+    
+    if(determinant > 0) {
+        r1 = (-b + sqrt(determinant))/(2*a);
+        r2 = (-b - sqrt(determinant))/(2*a);
+        printf("Distinct Real : %.2f , %.2f",r1,r2);
+    }
+    else if(determinant == 0) {
+        r1 = r2 = -b/(2*a);
+        printf("Equal Real: %.2f , %.2f",r1,r2);
+    }
+    else {
+        r1= -(b/(2*a));
+        r2= sqrt(-determinant)/(2*a);
+        printf("Complex: %.2f + i%.2f , %.2f - i%.2f",r1,r2,r1,r2);
+    }
+}
 
