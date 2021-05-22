2c9173cee4eb74c54e706585882131e4e27aa49d
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-05-22 10:09:27 +0530

    Update Day12b.c

--- Day12b.c
+++ Day12b.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+    int n,arr[10];
+    scanf("%d",&n);
+    for(int i = 0;i < n;i++) {
+        scanf("%d",&arr[i]);
+    }
+    printf("Entered array: ");
+    for(int i = 0;i < n;i++) {
+        printf("%d ",arr[i]);
+    }
+    printf("\n%d",arr[1]);
+  
+    return 0;
+}
