c24faa8bc13c7247d2e82cebb362f316f95c011a
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-05-25 00:26:30 +0530

    Update Day14a.c

--- Day14a.c
+++ Day14a.c
@@ -7,7 +7,7 @@ int main() {
     char str[100],a;
     int n,count = 0;
     scanf("%[^\n]%*c", str);
-    n= strlen(str);
+    n = strlen(str);
     scanf("%c",&a);
     for (int i = 0;i < n;i++) {
         if(a == str[i]) {
