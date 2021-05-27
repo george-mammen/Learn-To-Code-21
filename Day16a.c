1859871dc9e2bd59463e78c0e253021a412517bc
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-05-27 09:37:04 +0530

    Update Day16a.c

--- Day16a.c
+++ Day16a.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int main() {
+    int m,n,arr[10][10],num,temp = 0;
+    scanf("%d %d",&m,&n);
+    for(int i = 0; i < m;i++) {
+        for(int j = 0; j < n;j++) {
+            scanf("%d",&arr[i][j]);
+       }
+    }
+    scanf("%d",&num);
+    for(int i = 0; i < m;i++) {
+        for(int j = 0; j < n;j++) {
+            if (arr[i][j] == num) {
+                printf("Element found at (%d,%d)\n",i,j);
+                temp++;
+            }
+        }
+    }
+    if(temp == 0) {
+        printf("Element not found");
+    }
+}
