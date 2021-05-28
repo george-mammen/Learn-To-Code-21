ca8c8ec488a0334f7a17f76a4b8d331b3f8a09cb
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-05-28 11:20:14 +0530

    Update Day17b.c

--- Day17b.c
+++ Day17b.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    int n,max,min;
+    scanf("%d",&n);
+    int arr[n];
+    
+    for(int i = 0;i < n;i++) {
+        scanf("%d",&arr[i]);
+    }
+    max = arr[0];
+    min = arr[0];
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    printf("%d", min);
+    printf("\n%d", max);
+}
 
