ab13584dfbb0e7593de5ab7d7ac83834d22614d0
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-06-02 16:34:47 +0530

    Update Day20b.c

--- Day20b.c
+++ Day20b.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
     
 char str[100];
+
 void revstr(char str[]) {
         for(int i = strlen(str)-1;i >= 0 ;i--)
     {
@@ -12,17 +13,22 @@ void revstr(char str[]) {
 }
 
 void words(char str[]) {
-    int word = 1;
+    int word = 0;
     for(int i = 0; i < strlen(str);i++) {
-        if(str[i] == ' '  || str[i] == '\n') {
-            word++;
+            if(str[i] == ' '  || str[i] == '\n') {
+                word++;
+            }
         }
+    if(word == 0) {
+        printf("no of words = %d\n",word);
     }
-    printf("no of words = %d\n",word);
+    else {
+        printf("no of words = %d\n",word + 1);
+    }
 }
 
 int main() {
     scanf("%[^\n]%*c", str);
     words(str);
     revstr(str);
-}
+}}
