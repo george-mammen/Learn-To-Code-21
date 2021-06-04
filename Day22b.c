7f86124b9c02b5069972369ab519aa46047ea17a
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-06-04 09:51:02 +0530

    Update Day22b.c

--- Day22b.c
+++ Day22b.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+    char str[100];
+    scanf("%[^\n]%*c", str);
+    int vcount = 0,ccount = 0,scount = 0;
+    for(int i = 0;i < strlen(str);i++)
+    {
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+            vcount++;
+        else if(str[i] == ' ')
+            scount++;
+        else 
+            ccount++;
+    }
+    printf("%d\n%d\n%d",vcount,ccount,scount);
+}
