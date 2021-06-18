f9f8d2d6c90a4d793cbf7c60b56286f0ff7ac704
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-06-19 00:56:00 +0530

    Update Day24c.c

--- Day24c.c
+++ Day24c.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+
+    int n,arrsum = 0,primesum = 0,count;
+    scanf("%d",&n);
+    int arr[n];
+    
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+        arrsum += arr[i];
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        count=0;
+        for(int j = 2; j <= arr[i]; j++)
+        {
+          if(arr[i] % j == 0)
+          {
+              count++;
+          }
+        }
+        if(count == 1)
+        {
+          primesum += arr[i];  
+        }
+    }
+    
+    printf("%d",arrsum * (n - primesum));
+    return 0;
+}
