e5ed7a99829b85930ccf571c3ce3f06a0cc47d8e
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-06-19 14:42:01 +0530

    Update Day24e.c

--- Day24e.c
+++ Day24e.c
@@ -4,23 +4,24 @@
 #include <stdlib.h>
 
 int main() {
-  int n;
-  int a = 0,b = 1,c = 1,temp;
-  scanf("%d",&n);
-    
-  for(int i = 2;i <= n-2;i++) {
-    temp = 0;
-    a = b;
-    b = c;
-    c = a + b;
-    for(int j = 2;j <= c/2;j++) {
-      if (c % j == 0) {
-        temp = 1;
-        break;
-      }
-    }
-    if(temp == 0) {
-      printf("%d is a fibo-prime.\n",c);
+   int n,a = 0,b = 1,c = 1,flag;
+   scanf("%d",&n);
+   for(int i = 2;i <= n-2;i++)
+    { 
+     flag = 0;
+     a = b;
+     b = c;
+     c = a + b;
+     for(int j = 2; j <= c/2;j++)
+        {
+       if(c % j == 0)
+        {
+            flag = 1;
+            break;
+        }
+        }
+         if(flag == 0)
+             printf("%d is a fibo-prime.\n",c);
     }
     return 0;
 }
