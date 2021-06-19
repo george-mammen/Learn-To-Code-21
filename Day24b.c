10c083257a48b35f37d046e317b6f2db205302ab
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2021-06-19 13:28:55 +0530

    Update Day24b.c

--- Day24b.c
+++ Day24b.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+    int numqs,maxdiff,count = 0,score = 0;
+    scanf("%d %d",&numqs,&maxdiff);
+    int arr[numqs];
+    for(int i = 0; i < numqs; i++) {
+        scanf("%d",&arr[i]);
+        if(count <= 1 && arr[i] <= maxdiff) {
+            score++;
+        }
+        else if (count > 1){
+            break;
+        }
+        else {
+            count++;
+        }
+    }
+    printf("Score = %d",score);
+    return 0;
+}
